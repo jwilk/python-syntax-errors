9527fdc18b78e79e67a47088447772e09a6e5751
Anders Kaseorg <andersk@mit.edu>
2022-06-06 15:39:41 -0700

    since-3.11: Simplify by testing except* instead
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- since-3.11.py
+++ since-3.11.py
@@ -1,3 +1,4 @@
-match 0:  # Python >= 3.11 is required
-    case 1:
-        async def f(x): [[x async for x in ...] for y in ()]  # Python >= 3.11 is required
+try:
+    pass
+except* ():  # Python >= 3.11 is required
+    pass
